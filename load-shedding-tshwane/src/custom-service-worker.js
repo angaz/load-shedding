4be5c8f75aa9ddbddaab25ba7d2f3e4ec2a42261
Angus Dippenaar <angusdippenaar@gmail.com>
2018-12-16 12:04:01 +0200

    Some console logging for push event

--- load-shedding-tshwane/src/custom-service-worker.js
+++ load-shedding-tshwane/src/custom-service-worker.js
@@ -1,12 +1,15 @@
 /* eslint no-restricted-globals: 0 */
 
 self.addEventListener('push', event => {
+  console.log('A push event fired!');
+
   const eventJson = event.data.json();
+  console.log(eventJson);
 
   event.waitUntil(
     self.registration.showNotification(
       eventJson.title,
-      eventJson.options
+      eventJson.options,
     )
   );
 });
