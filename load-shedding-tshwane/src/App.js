4ebcf21914e79c4f3dd7d9b2f4711967cce38ff3
Angus Dippenaar <angusdippenaar@gmail.com>
2019-02-12 17:44:39 +0200

    Convert App to function. No more classes

--- load-shedding-tshwane/src/App.js
+++ load-shedding-tshwane/src/App.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 import LoadShedding from './components/LoadShedding/LoadShedding';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LoadShedding />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <div className="App">
+    <LoadShedding />
+  </div>
+);
